61b44298111f0813a9e44352ec480952a3a04ece
FiTTrA990 <fitravg5@gmail.com>
2025-09-24 16:15:05 +0700

    Add files via upload

---
+++ CPP/Elektronik.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+class Elektronik {
+protected:
+    int id;
+    string nama, merek, harga, gambar;
+
+public:
+    Elektronik() : id(0), nama(""), merek(""), harga(""), gambar("") {}
+    Elektronik(int i, string n, string m, string h, string g)
+        : id(i), nama(n), merek(m), harga(h), gambar(g) {}
+
+    int getId() const{ 
+        return id; 
+    }
+    string getNama() const{ 
+        return nama; 
+    }
+    string getMerek() const{ 
+        return merek; 
+    }
+    string getHarga() const{ 
+        return harga; 
+    }
+    string getGambar() const{ 
+        return gambar; 
+    }
+
+    void setId(int i){ 
+        id = i; 
+    }
+    void setNama(string n){ 
+        nama = n; 
+    }
+    void setMerek(string m){ 
+        merek = m; 
+    }
+    void setHarga(string h){ 
+        harga = h; 
+    }
+    void setGambar(string g){ 
+        gambar = g; 
+    }
+};
